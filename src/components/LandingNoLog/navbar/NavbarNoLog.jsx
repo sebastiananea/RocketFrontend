0a62d204ea4c1c718351bea3925497b368ae24e9
Ignacio Aranda <87491116+elnacho02@users.noreply.github.com>
2021-12-05 17:39:58 -0300

    _redirects mod

--- src/components/LandingNoLog/navbar/NavbarNoLog.jsx
+++ src/components/LandingNoLog/navbar/NavbarNoLog.jsx
@@ -54,7 +54,7 @@ function NavbarNoLog(props) {
                         onClick={()=>{
                         if(localStorage.getItem("token")) return history.push("/trueHome")
                         else return history.push("/signin")
-                    }}>ESTUDIANTES</button>
+                    }}>STUDENT</button>
                 </div>
               
         </div>
